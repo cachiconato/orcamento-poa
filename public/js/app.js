5d65c3094fd5d86db9d11fa261d69594e6c29b29
chiconato <cachiconato@gmail.com>
2013-07-25 15:53:29 -0300

    Adicionando grafico treemap

--- public/js/app.js
+++ public/js/app.js
@@ -5,15 +5,23 @@ function HomeController($scope, $http) {
   $http.get('/agencies').success(function(data) {
     var chartData = new google.visualization.DataTable();
     chartData.addColumn('string', 'Agencia');
+    chartData.addColumn('string', 'Parent');
     chartData.addColumn('number', 'Valor');
+    chartData.addColumn('number', 'increase/decrease (color)');
+
+    chartData.addRow(['Agencias', null, 0, 0]);
 
     data = data.splice(0, 5);
-    _.each(data, function(agencia) { chartData.addRow([agencia.name, agencia.total_spent]) });
-    var chart = new google.visualization.PieChart(document.getElementById('chart_orcamento'));
-    var options = {'title':'Agencias',
-                       'width':800,
-                       'height':600};
-
+    _.each(data, function(agencia) { chartData.addRow([agencia.name, 'Agencias',  agencia.total_spent, 10]) });
+    var chart = new google.visualization.TreeMap(document.getElementById('chart_orcamento'));
+    var options = {
+          minColor: '#f00',
+          midColor: '#ddd',
+          maxColor: '#0d0',
+          headerHeight: 15,
+          fontColor: 'black',
+          showScale: true
+    };
     var formatter = new google.visualization.NumberFormat( {prefix: 'R$', negativeColor: 'red', negativeParens: true});
     formatter.format(chartData, 1);
 
